77f8f71e3eed07f64d442d0511126fd766434b28
Yevhenii Kalinichenko <djek195@gmail.com>
2023-10-31 20:39:21 +0200

    just commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
-  return <h1>Hello React!!!! test task</h1>;
+  return <h1>Hello React!!!!</h1>;
 }
 
 //React ver 18
